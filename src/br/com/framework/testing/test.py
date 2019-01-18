ae313bc08476f14e2b4322e61fbb677f340b449f
Gabriel Aguido Fraga <gabriel_aguido@hotmail.com>
2019-01-18 20:01:26 -0200

    initial commit

---
+++ src/br/com/framework/testing/test.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def test_open_browser(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    if __name__ == '__main__':
+        unittest.main()
